e1908c74ac147b67a44a7b3947f08060c8b17dbf
Jessica Madamba <jessica.madamba@gmail.com>
2019-02-26 10:22:34 -0800

    Implement final proxy server

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/:productId', express.static(path.join(__dirname, 'public')));
 
 const proxyTable = {
   '/products': 'http://ec2-3-83-95-221.compute-1.amazonaws.com',
-  '/**/reviews': 'http://ec2-18-223-116-197.us-east-2.compute.amazonaws.com'
+  '/recently-viewed': 'http://ec2-3-17-36-143.us-east-2.compute.amazonaws.com/',
+  '/reviews': 'http://ec2-18-188-165-52.us-east-2.compute.amazonaws.com'
 }
 
 const options = {
